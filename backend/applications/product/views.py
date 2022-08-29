bf6b66cc3d8c875d7b305d6ddd3ea51b9bff70c0
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 11:10:51 +0300

    changed static data system to persistency

--- backend/applications/product/views.py
+++ backend/applications/product/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .products import products
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view(["GET"])
@@ -24,14 +26,13 @@ def get_routes(request):
 
 @api_view(["GET"])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
 def get_product(request, pk):
-    product = None
-    for i in products:
-        if i["_id"] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
