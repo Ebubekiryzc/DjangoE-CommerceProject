23b7fe8207cd8ab365e615811e5572a769676438
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 14:38:27 +0300

    added cart reducer

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -5,12 +5,18 @@ import rootReducer from "./rootReducer";
 
 const middleware = [thunk];
 
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
 const initialState = {
-}
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 export function configureStore() {
-    return createStore(rootReducer,
-        initialState,
-        composeWithDevTools(applyMiddleware(...middleware)),
-    );
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
 }
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,10 +3,12 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
