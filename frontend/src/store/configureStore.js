0c5562c8cc536feba7febcbe7ec4b87a2f72c1d5
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 20:02:23 +0300

    user state management created

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -9,8 +9,13 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 export function configureStore() {
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -4,11 +4,13 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 export default rootReducer;
