16195f0d3412964a0c3f651419db7f10de45af5d
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 13:08:51 +0300

    removed unnecessary vars

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -3,12 +3,9 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 
-const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
-
 const middleware = [thunk];
 
 const initialState = {
-    userLogin: { userInfo: userInfoFromStorage }
 }
 
 export function configureStore() {
