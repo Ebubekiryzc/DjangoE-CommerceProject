09e7416beb88d78bfed3d5fb4ab546cbeb626adf
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 18:50:01 +0300

    refactored url paths

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("applications.product.urls"))
+    path('api/routes', views.get_routes, name="routes"),
+    path("api/products/", include("applications.product.urls")),
+    path("api/users/", include("applications.account.urls"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
