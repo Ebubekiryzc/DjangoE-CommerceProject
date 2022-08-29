627c94d5ce8a38437cc19211d49235e83a1e7897
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-29 13:08:30 +0300

    removed react strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,7 @@ const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider>
 );
 
