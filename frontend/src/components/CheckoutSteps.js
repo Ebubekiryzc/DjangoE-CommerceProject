b6c293e2d3c4421aff6686c764dd50df340228e8
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-30 11:47:11 +0300

    fixed visualization

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -13,6 +13,8 @@ function CheckoutSteps({ step1, step2, step3, step4 }) {
         ) : (
           <Nav.Link disabled>Login</Nav.Link>
         )}
+      </Nav.Item>
+      <Nav.Item>
         {step2 ? (
           <LinkContainer to="/shipping">
             <Nav.Link>Shipping</Nav.Link>
@@ -20,6 +22,8 @@ function CheckoutSteps({ step1, step2, step3, step4 }) {
         ) : (
           <Nav.Link disabled>Shipping</Nav.Link>
         )}
+      </Nav.Item>
+      <Nav.Item>
         {step3 ? (
           <LinkContainer to="/payment">
             <Nav.Link>Payment</Nav.Link>
@@ -27,6 +31,8 @@ function CheckoutSteps({ step1, step2, step3, step4 }) {
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
         )}
+      </Nav.Item>
+      <Nav.Item>
         {step4 ? (
           <LinkContainer to="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
