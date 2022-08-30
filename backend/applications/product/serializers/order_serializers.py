bc3981f7a6888d05e919c4903faff0d329103d73
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-30 21:12:31 +0300

    fixed minor bug

--- backend/applications/product/serializers/order_serializers.py
+++ backend/applications/product/serializers/order_serializers.py
@@ -31,7 +31,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_shippingAddress(self, obj):
         try:
             address = ShippingAddressSerializer(
-                obj.shippingAddress, many=False)
+                obj.shippingaddress, many=False).data
         except:
             address = False
         return address
