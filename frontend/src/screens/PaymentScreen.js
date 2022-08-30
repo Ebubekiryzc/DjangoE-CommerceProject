84118b2847dc05a8c80b8a139ba471c779521785
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-08-30 12:02:11 +0300

    created payment screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Form, Button, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+// import { savePaymentMethod } from "../store/actions/cartActions";
+
+function PaymentScreen({ history }) {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const dispatch = useDispatch();
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  if (!shippingAddress.address) {
+    history.push("/shipping");
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="my-3">
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="paypal"
+              name="paymentMethod"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}>
+
+              </Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Button type="button" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default PaymentScreen;
