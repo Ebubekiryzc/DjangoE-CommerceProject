cd5e5c9770a7d9a129ebd10f42bfdda2e27a2bd5
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 12:33:56 +0300

    added displaying owned orders functionality

--- backend/applications/product/urls/order_urls.py
+++ backend/applications/product/urls/order_urls.py
@@ -3,6 +3,7 @@ from applications.product.views import order_views
 
 urlpatterns = [
     path("add/", order_views.add_order_items, name="orders-add"),
+    path("myorders/", order_views.get_my_orders, name="my-orders"),
     path("<str:pk>/", order_views.get_order_by_id, name="user-order"),
     path("<str:pk>/pay/", order_views.pay_order, name="pay-order")
 ]
