df1c1509a2257b326ef9fd4fd504d540f741200d
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 12:03:07 +0300

    added new url paths for views

--- backend/applications/product/urls/order_urls.py
+++ backend/applications/product/urls/order_urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from applications.product.views import order_views
-from applications.product.views.order_views import get_order_by_id
-from backend.applications.product.views.order_views import update_order_to_paid
 
 urlpatterns = [
     path("add/", order_views.add_order_items, name="orders-add"),
     path("<str:pk>/", order_views.get_order_by_id, name="user-order"),
-    path("<str:pk>/pay/", order_views.update_order_to_paid, name="pay-order")
+    path("<str:pk>/pay/", order_views.pay_order, name="pay-order")
 ]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
+from applications.product.views import order_views
 
 
 urlpatterns = [
@@ -11,7 +12,8 @@ urlpatterns = [
     path('api/routes', views.get_routes, name="routes"),
     path("api/products/", include("applications.product.urls.product_urls")),
     path("api/orders/", include("applications.product.urls.order_urls")),
-    path("api/users/", include("applications.account.urls"))
+    path("api/users/", include("applications.account.urls")),
+    path("api/webhook/", order_views.stripe_webhook_view, name="stripe-webhook")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
