364ee1367998904b1a0b2cc1a6f7d3879588681f
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 15:58:44 +0300

    added update and create for products

--- backend/applications/product/urls/product_urls.py
+++ backend/applications/product/urls/product_urls.py
@@ -3,6 +3,8 @@ from applications.product.views import product_views
 
 urlpatterns = [
     path("", product_views.get_products, name="products"),
+    path("create/", product_views.create_product, name="product-create"),
     path("delete/<str:pk>/", product_views.delete_product, name="product-delete"),
+    path("update/<str:pk>/", product_views.update_product, name="product-update"),
     path("<str:pk>/", product_views.get_product, name="product")
 ]
--- backend/applications/product/views/product_views.py
+++ backend/applications/product/views/product_views.py
@@ -26,3 +26,40 @@ def delete_product(request, pk):
     product = Product.objects.get(_id=pk)
     product.delete()
     return Response("Product Deleted")
+
+
+@api_view(["POST"])
+@permission_classes([IsAdminUser])
+def create_product(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name="Sample name",
+        price=0,
+        brand="Sample brand",
+        category="SampleCategory",
+        countInStock=0,
+        description=""
+    )
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAdminUser])
+def update_product(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data["name"]
+    product.price = data["price"]
+    product.brand = data["brand"]
+    product.countInStock = data["countInStock"]
+    product.category = data["category"]
+    product.description = data["description"]
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
