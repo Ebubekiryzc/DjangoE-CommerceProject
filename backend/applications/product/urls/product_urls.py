883ee9e7153191720f179c76df5b7b941b67e459
Ebubekir YAZICI <79103394+Ebubekiryzc@users.noreply.github.com>
2022-09-03 15:28:26 +0300

    added product deletion for admins usage

--- backend/applications/product/urls/product_urls.py
+++ backend/applications/product/urls/product_urls.py
@@ -3,5 +3,6 @@ from applications.product.views import product_views
 
 urlpatterns = [
     path("", product_views.get_products, name="products"),
+    path("delete/<str:pk>/", product_views.delete_product, name="product-delete"),
     path("<str:pk>/", product_views.get_product, name="product")
 ]
--- backend/applications/product/views/product_views.py
+++ backend/applications/product/views/product_views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from applications.product.models import Product
@@ -17,3 +18,11 @@ def get_product(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAdminUser])
+def delete_product(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response("Product Deleted")
